ee00db9f261d20da3aa094f55df094736832f961
eoliva50 <eoliva50>
2023-11-12 17:42:06 -0600

    Base

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
-  import NavMenu from '@/components/NavMenu.vue'
+  import NavBar from '@/components/NavBar.vue'
 </script>
 
 <template>
-  <NavMenu />
+  <NavBar />
   <RouterView />
 </template>
--- src/router.js
+++ src/router.js
@@ -1,15 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomePage from '@/views/HomePage.vue'
+import { useAuth } from '@/composables/useAuth'
+const { isAuthenticated } = useAuth()
+
+import MainPage from '@/components/MainPage.vue'
+import LoginPage from '@/components/LoginPage.vue'
+import SettingsPage from '@/components/SettingsPage.vue'
+import NotFound from '@/components/NotFound.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/', name: 'Home', component: MainPage },
+  { path: '/login', name: 'Login', component: LoginPage },
+  { path: '/settings', name: 'Settings', component: SettingsPage, meta: { requiresAuth: true } },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to, _, next) => {
+  if (to.meta.requiresAuth && !isAuthenticated.value) next({ name: 'Login', query: { redirect: to.fullPath } })
+  else next()
 })
 
 export default router
